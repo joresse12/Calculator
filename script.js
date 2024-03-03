8dae160926a8b1799a35a201a27c11b896786de8
Joresse Zekeng <inf4016@hs-worms.de>
2024-03-03 14:12:30 +0100

    update and bug fixing

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ const display_field = document.getElementById('display_field');
 
 function displayNumber(val) {
     display_field.value += val;
+    console.log(display_field.value);
 }
+function clearAll() {
+     display_field.value = ' ';
+}
+function clearOne() { 
+    display_field.value = display_field.value.toString().splice(0, -1);
+}
+function equal() {
+     display_field.value = eval(display_field.value).toString();
+ }
